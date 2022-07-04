a78fe59f17927d19241f402f98d43fdb805db9c7
SasaKrstic <krlefcrs@hotmail.com>
2022-07-04 15:57:31 +0200

    do 20. lekcije fetch errors

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,31 @@ const Home = () => {
   
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
-  
+  const [error, setError] = useState(null);
+
   const [name, setName] = useState('Mario');
 
   useEffect(() => {
     setTimeout(() => {
       
     fetch('http://localhost:8000/blogs')
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      setBlogs(data);
-      setIsPending(false);
-    });
+      .then(res => {
+        console.log(res);
+        if (!res.ok){
+          throw Error('Ne mozemo fetchovati podatke za taj izvor');
+        }
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+        
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
   }, 1000);
   }, []);
   
@@ -26,6 +37,7 @@ const Home = () => {
     
     return(
       <div className="home">
+          {error && <div>{ Error } </div>}
           {isPending && <div>LOADING...</div>}
           {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
           <button onClick={() => setName('Nenad')}>Promeni ime</button>
