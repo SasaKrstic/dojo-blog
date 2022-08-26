d4f46d57a42920359faecd9d6942f150542ff926
SasaKrstic <krlefcrs@hotmail.com>
2022-08-26 12:30:42 +0200

    29.z Post request, dodavanje bloga preko forme na home page

--- src/Create.js
+++ src/Create.js
@@ -5,19 +5,30 @@ const Create = () => {
   const [ body, setBody] = useState('');
   const [ author, setAuthor] = useState('Mario');
   
+  const [isPending, setIsPending] = useState(false);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    
     const blog = { title, body, author };
     
-    console.log(blog);
+    setIsPending(true);
+    
+    // console.log(blog);
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    })
   }
   
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
       <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
         <input
           type="text"
           required
@@ -38,7 +49,9 @@ const Create = () => {
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+       { !isPending && <button>Add Blog</button> }
+       { isPending && <button>Adding Blog...</button> }
+        
         &nbsp;
         <p>{title}</p>
         <p>{body}</p>
